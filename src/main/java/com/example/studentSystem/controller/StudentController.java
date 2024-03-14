530b17455a89fe0627d0f8d64ea386df00c08068
Liviru-Navaratna <livirunavaratna@gmail.com>
2024-03-14 10:15:59 +0530

    Implemented the front end with POST and GET methods

--- src/main/java/com/example/studentSystem/controller/StudentController.java
+++ src/main/java/com/example/studentSystem/controller/StudentController.java
@@ -1,5 +1,5 @@
 package com.example.studentSystem.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.studentSystem.model.Student;
 import com.example.studentSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins = "http://localhost:3000") // Update with your React app's URL
 public class StudentController {
     @Autowired
     private StudentService studentService;
--- studentSystem/student-front-end/src/components/Student.js
+++ studentSystem/student-front-end/src/components/Student.js
@@ -3,12 +3,36 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {Button} from '@mui/material';
 
 export default function Student() {
     const paperStyle={padding:'50px', width:'600px', margin: '20px auto'}
     const[name,setName]=useState('');
     const[address,setAddress]=useState('');
+    const[student, setStudent] = useState([]);
+
+    const handleClick = (e)=>{
+      e.preventDefault();
+      const student={name, address}
+      console.log(student);
+      fetch("http://localhost:8080/student/add", 
+      { method:"POST", 
+        headers:{"Content-Type":"application/json"}, 
+        body:JSON.stringify(student)})
+        .then(()=>{
+        console.log("new student added")
+      })
+    }
+
+    useEffect(()=>
+    {
+      fetch("http://localhost:8080/student/getAll")
+      .then(response=>response.json())
+      .then((result)=>{
+        setStudent(result);
+      })
+    },[])
   return (
     <Container>
         <Paper elevation={3} style={paperStyle}>
@@ -27,7 +51,22 @@ export default function Student() {
       <TextField id="standard-basic" label="Student Address: " variant="standard" fullWidth
       value={address}
       onChange={(e)=>setAddress(e.target.value)}/>
+
+      <Button variant="contained" color="secondary" onClick={handleClick}>Save data</Button>
+      
     </Box>
+    </Paper>
+      <h1>Students List</h1>
+    <Paper elevation={3} style={paperStyle}>
+      {student.map(student =>  (
+        <Paper elevation={6} style={{margin: "10px", padding: "15px", textAlign: "left"}} key={student.id}>
+          Id: {student.id}<br/>
+          Name: {student.name}<br/>
+          Address: {student.address}
+        </Paper>
+      )
+
+      )}
     </Paper>
     </Container>
   );
